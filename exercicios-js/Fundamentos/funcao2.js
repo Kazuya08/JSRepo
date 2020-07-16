9ed8ad8fd54e816d17dd31177f2aec6d53fea8dd
Kazuya08 <kazuyathiago@hotmail.com>
2020-07-16 10:37:03 -0300

    add funcao2.js

---
+++ exercicios-js/Fundamentos/funcao2.js
@@ -0,0 +1,26 @@
+// Armazenando uma funcao em uma variavel
+const imprimirSoma = function (a, b){
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+
+// Armazenando uma funcao arrow em uma variavel
+const soma = (a, b) => {
+    return a + b
+}
+
+console.log(soma(2, 3))
+
+// retorno implícito 
+const subtracao = (a, b) => a - b
+console.log(subtracao(2, 3))
+
+const imprimir2 = a => console.log(a)
+imprimir2('Legal!!!')
+
+//testando
+const multiplicacao = (a = 0, b = 0) => {
+    return a * b
+}
+console.log(multiplicacao(2, 2))
