14c0828b9151ab0e7b1241e17976dded90475a14
Kazuya08 <kazuyathiago@hotmail.com>
2020-07-17 17:52:24 -0300

    add usandoVar2.js

---
+++ exercicios-js/Fundamentos/usandoVar2.js
@@ -0,0 +1,10 @@
+var numero = 1
+{
+     var numero = 2
+     console.log('dentro =', numero)
+}
+console.log('fora =', numero)
+
+/* o 1 foi sobreescrito para 2 quando declarei a segunda vez 
+então aparecera 2 nos dois consoles
+*/
