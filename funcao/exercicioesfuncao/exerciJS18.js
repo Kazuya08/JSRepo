ec3bfe456ddbce30c0710f40931fb06efd06f98e
Kazuya08 <kazuyathiago@hotmail.com>
2020-10-20 11:25:37 -0300

    add exerciJS18.js

---
+++ funcao/exercicioesfuncao/exerciJS18.js
@@ -0,0 +1,34 @@
+function extenso(number) {
+    switch (number) {
+        case 0: 
+            return "Zero"
+        case 1:
+            return "Um"
+        case 2: 
+            return "Dois"
+        case 3:
+            return "Três"
+        case 4:
+            return "Quatro"
+        case 5:
+            return "Cinco"
+        case 6:
+            return "Seis"
+        case 7: 
+            return "Sete"
+        case 8:
+            return "Oito"
+        case 9:
+            return "Nove"
+        case 10:
+            return "Dez"
+        default:
+            return "Número fora do intervalo" 
+    }
+}
+
+console.log(extenso(5));
+
+console.log(extenso(11));
+
+console.log(extenso(10))
