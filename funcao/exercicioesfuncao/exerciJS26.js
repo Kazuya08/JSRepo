0b7e5084fb64462232aa9b06e6c87737826005c5
Kazuya08 <kazuyathiago@hotmail.com>
2020-12-03 10:02:36 -0300

    add exerciJS26.js

---
+++ funcao/exercicioesfuncao/exerciJS26.js
@@ -0,0 +1,22 @@
+function pares() {
+    i = 1
+    while (i < 101) {
+        if(i % 2 == 0) {
+            console.log(i)   
+        }
+        i++
+    }
+}
+
+pares()
+
+//Com FOR
+/*function pares() {
+    for (let i = 1; i <= 100; i++) {
+        if(i % 2 == 0) {
+            console.log(i)
+        }
+    }
+}
+ pares()
+ */
