dc4fe9e10e15ff211eb04e55e4edf65bdb466501
Kazuya08 <kazuyathiago@hotmail.com>
2021-02-03 10:27:18 -0300

    add 20AreaDoTriangulo.js

---
+++ ListaExercicios2/20AreaDoTriangulo.js
@@ -0,0 +1,9 @@
+function AreaTriangulo(base, altura){
+    const area = (base * altura) / 2
+    return area.toFixed(2) 
+}
+
+console.log(AreaTriangulo(10, 15))
+console.log(AreaTriangulo(7, 9))
+console.log(AreaTriangulo(9.25, 13.1))
+
