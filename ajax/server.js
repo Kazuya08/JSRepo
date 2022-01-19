c2579ee843d71ca33fe0459964ca296558340704
kazuya08 <kazuyathiago@hotmail.com>
2022-01-19 10:01:17 -0300

    add AJAX httpRequest1.html

--- ajax/server.js
+++ ajax/server.js
@@ -8,8 +8,8 @@ app.use(express.static("."));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-app.get("/teste", (req, res) => {
-  res.send("OKAY");
-});
+// app.get("/teste", (req, res) => {
+//   res.send("OKAY");
+// });
 
 app.listen(port, () => console.log(`Executando Server na porta ${port}`));
